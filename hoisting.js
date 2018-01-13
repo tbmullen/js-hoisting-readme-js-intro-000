d0cba8b18357eaf59bd933275b09872a22625df0
Trevor Mullen <tbhackworth@gmail.com>
2018-01-13 20:32:30 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -20,14 +20,13 @@ function crazy() {
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
-
   function sayMy() {
     console.log(name);
     var name = "Kristin";
   }
 
   sayMy();
-
+  
+  var name = "Cricky";
 
 }
